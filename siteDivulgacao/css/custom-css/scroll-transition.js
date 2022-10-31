38be1c9ef4368e0c43371c69748397986fea7969
Alyssonric <alyssonric@hotmail.com>
2022-10-31 16:34:40 -0300

    Preparando

---
+++ siteDivulgacao/css/custom-css/scroll-transition.js
@@ -0,0 +1,9 @@
+$(function () {
+    $(window).on('scroll', function () {
+        if ( $(window).scrollTop() > 10 ) {
+            $('.nav').addClass('active');
+        } else {
+            $('.nav').removeClass('active');
+        }
+    });
+});
