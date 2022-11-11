0a172c05281feb01068c5ac2325bfc0647f11c55
Alyssonric <alyssonric@hotmail.com>
2022-11-11 15:58:28 -0300

    Ajustes

--- site-teste/js/scroll.js
+++ site-teste/js/scroll.js
@@ -1,12 +1,12 @@
 $(function () {
     $(window).on('scroll', function () {
         if ($(window).scrollTop() > 30) {
-            $('.nav-link').removeClass('text-light');
+            $('.nav-link').removeClass('text-white');
             $('.nav-link').addClass('text-dark');
             $('#menu').addClass('active');
         } else {
             $('#menu').removeClass('active');
-            $('.nav-link').addClass('text-light');
+            $('.nav-link').addClass('text-white');
             $('.nav-link').removeClass('text-dark');
         }
     });
