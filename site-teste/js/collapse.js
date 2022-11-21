212f35c9e5810ef00b40695805a114084468ccfe
Alyssonric <alyssonric@hotmail.com>
2022-11-21 13:17:54 -0300

    .

---
+++ site-teste/js/collapse.js
@@ -0,0 +1,7 @@
+$(".nav-button").click(function () {
+    $(".nav-button,.primary-nav").toggleClass("open");
+});  
+
+$(".scroll-link").click(function () 
+    $(".nav-button,.primary-nav").toggleClass("close");
+});
