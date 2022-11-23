31c1370faaeb7a234b82038ff03bdd8b820d5750
Alyssonric <alyssonric@hotmail.com>
2022-11-23 15:58:45 -0300

    teste

--- site-teste/js/collapse.js
+++ site-teste/js/collapse.js
@@ -1,7 +1,16 @@
-$(".nav-button").click(function () {
-    $(".nav-button,.primary-nav").toggleClass("open");
-});  
+function menuShow() {
+    let but1 = documet.querySelector('navBar-toggler')
+    if (but1.classList.contains('open')) {
+        but1.classList.remove('open');
+    } else {
+        but1.classList.add('open');
+    }
+}
 
-$(".scroll-link").click(function () 
-    $(".nav-button,.primary-nav").toggleClass("close");
-});
+// $(".nav-button").click(function () {
+//     $(".nav-button,.primary-nav").toggleClass("open");
+// });  
+
+// $(".scroll-link").click(function () 
+//     $(".nav-button,.primary-nav").toggleClass("close");
+// });
